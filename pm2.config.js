f86dcd419497bab559dbd3b96f0422289c54d55c
Roman Usatenko <roman@usatenko.net>
2024-01-04 17:41:21 -0800

    Minimize pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -3,11 +3,9 @@ module.exports = {
         {
             name: 'my-little-chat-app',
             script: 'index.js',
-            watch: false,
             env: {
                 NODE_ENV: 'production'
-            },
-            post_update: ['npm install'],
+            }
         }
     ]
 };
