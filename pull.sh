78cd48dfffde429ba808b253d853f88778590c3b
Roman Usatenko <roman@usatenko.net>
2024-01-04 21:19:00 -0800

    Fix pull script

--- pull.sh
+++ pull.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "=============== Pulling changes ==============="
+cd "$(dirname "$0")"
 if git pull origin main; then
   npm install
   pm2 restart my-little-chat-app
+else
+  echo "No changes"
 fi
